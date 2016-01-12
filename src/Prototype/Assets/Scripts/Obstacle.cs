2939e01bea4fd3062d52f2bb93836b6cfa394a61
dka271 <dka271@vt.edu>
2016-01-12 18:27:42 -0500

    Moving the code for Again!! to my GitHub from backups.

---
+++ src/Prototype/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+	public Color trueColor;
+	private bool isVisible;
+
+	public void ShowTrueColor() {
+		if (!isVisible) {
+			this.renderer.material.color = trueColor;
+			isVisible = true;
+		}
+	}
+}
---
+++ src/Prototype/Assets/Scripts/Tile.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		renderer.enabled = false;
+	}
+	
+	void OnParticleCollision(GameObject particleSystem) {
+		renderer.enabled = true;
+	}
+}
